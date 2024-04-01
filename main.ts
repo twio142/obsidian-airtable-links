1c8dd1fa5e012b4e35452cbae63a30c52cd98d83
twio142 <64556708+twio142@users.noreply.github.com>
2024-04-01 23:12:02 +0200

    first commit

--- main.ts
+++ main.ts
@@ -3,12 +3,17 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	accessToken: string;
+	linksTableUrl: string;
+	listsTableUrl: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	accessToken: '',
+	linksTableUrl: '',
+	listsTableUrl: '',
 }
+
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -17,23 +22,23 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		// 	new Notice('This is a notice!');
+		// });
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new SampleModal(this.app, this).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -56,7 +61,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new SampleModal(this.app, this).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -70,9 +75,9 @@ export default class MyPlugin extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -89,16 +94,66 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async GetAirtableLinks(listUrl: string) {
+		listUrl = listUrl.split('?')[0];
+		const {listsTableUrl, linksTableUrl, accessToken} = this.settings;
+		const listsMatch = listsTableUrl.match(this.AIRTABLE_URL_REGEX);
+		const linksMatch = linksTableUrl.match(this.AIRTABLE_URL_REGEX);
+		const match = listUrl.match(this.AIRTABLE_URL_REGEX);
+		if (!match || match[1] !== listsMatch[1] || match[2] !== listsMatch[2] || !match[4]) {
+			new Notice('Invalid list URL');
+			return;
+		}
+		const requestUrl = `https://api.airtable.com/v0/${linksMatch[1]}/${linksMatch[2]}?filterByFormula=FIND(%22${match[4]}%22%2C+%7BList-URLs%7D)`;
+		const response = await fetch(requestUrl, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
+		});
+		const data = await response.json();
+		const records = data?.records;
+		if (!records || records.length === 0) {
+			new Notice('No links found');
+			return;
+		}
+		return records;
+	}
+
+	AIRTABLE_URL_REGEX = /https:\/\/airtable.com\/(app[^/]+)\/(tbl[^/]+)(\/viw[^/]+\/(rec[^/]+))?/;
+
+	// async makeTable(listUrl: string) {
+	// 	let records = await this.GetAirtableLinks(listUrl);
+	// 	if (!records) return;
+	// }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
 		super(app);
+		this.plugin = plugin;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Enter Airtable List URL: ');
+		const input = contentEl.createEl('input');
+		input.focus();
+		input.onkeydown = (ev) => {
+			if (ev.key === 'Enter') {
+				this.plugin.GetAirtableLinks(input.value);
+				this.close();
+			}
+		};
+		contentEl.createEl('span').setText(' ');
+		const button = contentEl.createEl('button');
+		button.setText('Confirm');
+		button.onclick = () => {
+			this.plugin.GetAirtableLinks(input.value);
+			this.close();
+		}
 	}
 
 	onClose() {
@@ -123,14 +178,47 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Airtable Personal Access Token')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your token')
+				.setValue(this.plugin.settings.accessToken)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('Access Token: ' + value);
+					this.plugin.settings.accessToken = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Lists Table URL')
+			.setDesc('URL of the Airtable table containing lists')
+			.addText(text => text
+				.setPlaceholder('Enter table URL')
+				.setValue(this.plugin.settings.listsTableUrl)
+				.onChange(async (value) => {
+					value = value.split('?')[0];
+					if (!this.plugin.AIRTABLE_URL_REGEX.test(value)) {
+						new Notice('Invalid Airtable URL');
+						return;
+					}
+					console.log('Lists URL: ' + value);
+					this.plugin.settings.listsTableUrl = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Links Table URL')
+			.setDesc('URL of the Airtable table containing links')
+			.addText(text => text
+				.setPlaceholder('Enter table URL')
+				.setValue(this.plugin.settings.linksTableUrl)
+				.onChange(async (value) => {
+					value = value.split('?')[0];
+					if (!this.plugin.AIRTABLE_URL_REGEX.test(value)) {
+						new Notice('Invalid Airtable URL');
+						return;
+					}
+					console.log('Links URL: ' + value);
+					this.plugin.settings.linksTableUrl = value;
 					await this.plugin.saveSettings();
 				}));
 	}
---
+++ notes.md
@@ -0,0 +1,11 @@
+# Airtable Links Plugin
+
+## To-Dos
+
+- Watch for code blocks of a specific format
+- Formatting of the table:
+  - Displayed columns
+  - Sorting
+  - Filtering
+- [?] The rendered table should be editable
+  - Mark a row as done / not done
