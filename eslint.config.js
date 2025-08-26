3826e1f1aff13db17c62e4682e3070e83c016903
twio142 <64556708+twio142@users.noreply.github.com>
2025-08-26 15:31:46 +0200

    chore/style: set up linter; format code

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  formatters: {
+    css: false,
+    html: false,
+    json: false,
+    markdown: false,
+    yaml: false,
+  },
+  ignores: ['main.js'],
+}, {
+  files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+  rules: {
+    'style/semi': ['error', 'always'],
+    'style/brace-style': ['error', '1tbs'],
+    'no-console': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+    'node/prefer-global/process': 'off',
+  },
+});
--- main.ts
+++ main.ts
@@ -1,229 +1,231 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+/* eslint-disable no-new */
+import type { App } from 'obsidian';
+import { Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface AirtableLinksSettings {
-	accessToken: string;
-	baseID: string;
-	linksTableID: string;
-	listsTableID: string;
+  accessToken: string
+  baseID: string
+  linksTableID: string
+  listsTableID: string
 }
 
 interface CacheRecord {
-	links: Link[];
-	cachedAt: Date;
+  links: Link[]
+  cachedAt: Date
 }
 
 class Link {
-	name: string;
-	url: string;
-	list: string;
-	listURL?: string;
-	done: boolean;
-	created: string;
+  name: string;
+  url: string;
+  list: string;
+  listURL?: string;
+  done: boolean;
+  created: string;
 
-	constructor(data: { name: string; url: string; list: string; listURL: string; done: boolean; created: string }) {
-		this.name = data.name;
-		this.url = data.url;
-		this.list = data.list;
-		this.listURL = data.listURL;
-		this.done = data.done;
-		this.created = data.created;
-	}
+  constructor(data: { name: string, url: string, list: string, listURL: string, done: boolean, created: string }) {
+    this.name = data.name;
+    this.url = data.url;
+    this.list = data.list;
+    this.listURL = data.listURL;
+    this.done = data.done;
+    this.created = data.created;
+  }
 }
 
 class List {
-	name: string;
-	id: string;
-	url: string;
-	links?: string[];
+  name: string;
+  id: string;
+  url: string;
+  links?: string[];
 
-	constructor(data: { name: string; id: string, url: string, links?: string[] }) {
-		this.name = data.name;
-		this.id = data.id;
-		this.links = data.links || [];
-	}
+  constructor(data: { name: string, id: string, url: string, links?: string[] }) {
+    this.name = data.name;
+    this.id = data.id;
+    this.links = data.links || [];
+  }
 }
 
 const DEFAULT_SETTINGS: AirtableLinksSettings = {
-	accessToken: '',
-	baseID: '',
-	linksTableID: '',
-	listsTableID: '',
-}
+  accessToken: '',
+  baseID: '',
+  linksTableID: '',
+  listsTableID: '',
+};
 
 export default class AirtableLinks extends Plugin {
-	settings: AirtableLinksSettings;
+  settings: AirtableLinksSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		this.addSettingTab(new SettingTab(this.app, this));
-	}
+    this.addSettingTab(new SettingTab(this.app, this));
+  }
 
-	onunload() {
+  onunload() {
 
-	}
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-	async getAirtableLinks(listID: string) {
-		if (!this.REGEX.recordID.test(listID)) {
-			throw new Error('Invalid List ID');
-		}
-		let record = this.readCache(listID);
-		if (record) {
-			record.cachedAt = new Date();
-			return record.links;
-		}
-		const { baseID, linksTableID, accessToken } = this.settings;
-		let list = await this.getAirtableList(listID);
-		if (list.links.length === 0) {
-			throw new Error('List has no links');
-		}
-		const requestURL = `https://api.airtable.com/v0/${baseID}/${linksTableID}/listRecords`;
-		const response = await fetch(requestURL, {
-			method: "POST",
-			headers: {
-				"Authorization": `Bearer ${accessToken}`,
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				filterByFormula: `FIND(RIGHT({Record URL}, 17), "${list.links.join(',')}")`
-			})
-		});
-		const data = await response.json();
-		let links = data?.records;
-		if (!links || links.length === 0) {
-			throw new Error('No links found');
-		}
-		links = links.map((l: { fields: any; }) => l.fields);
-		links = links.map((l: { Name: any; URL: any; Done: any; Created: any; }) => new Link({ name: l.Name, url: l.URL, list: list.name, listURL: list.url, done: !!l.Done, created: l.Created }));
-		this.cache.set(listID, {
-			links,
-			cachedAt: new Date()
-		});
-		return links;
-	}
+  async getAirtableLinks(listID: string) {
+    if (!this.REGEX.recordID.test(listID)) {
+      throw new Error('Invalid List ID');
+    }
+    const record = this.readCache(listID);
+    if (record) {
+      record.cachedAt = new Date();
+      return record.links;
+    }
+    const { baseID, linksTableID, accessToken } = this.settings;
+    const list = await this.getAirtableList(listID);
+    if (list.links.length === 0) {
+      throw new Error('List has no links');
+    }
+    const requestURL = `https://api.airtable.com/v0/${baseID}/${linksTableID}/listRecords`;
+    const response = await fetch(requestURL, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        filterByFormula: `FIND(RIGHT({Record URL}, 17), "${list.links.join(',')}")`,
+      }),
+    });
+    const data = await response.json();
+    let links = data?.records;
+    if (!links || links.length === 0) {
+      throw new Error('No links found');
+    }
+    links = links.map((l: { fields: any }) => l.fields);
+    links = links.map((l: { Name: any, URL: any, Done: any, Created: any }) => new Link({ name: l.Name, url: l.URL, list: list.name, listURL: list.url, done: !!l.Done, created: l.Created }));
+    this.cache.set(listID, {
+      links,
+      cachedAt: new Date(),
+    });
+    return links;
+  }
 
-	async getAirtableList(listID: string) {
-		if (!this.REGEX.recordID.test(listID)) {
-			throw new Error('Invalid List ID');
-		}
-		const { baseID, listsTableID, accessToken } = this.settings;
-		const requestURL = `https://api.airtable.com/v0/${baseID}/${listsTableID}/${listID}`;
-		const response = await fetch(requestURL, {
-			headers: { Authorization: `Bearer ${accessToken}` }
-		});
-		const data = await response.json();
-		if (!data || !data.fields) {
-			throw new Error('List not found');
-		}
-		return new List({
-			name: data.fields.Name || 'Unnamed List',
-			id: data.id,
-			url: `https://airtable.com/${baseID}/${listsTableID}/${data.id}`,
-			links: data.fields.Links
-		});
-	}
+  async getAirtableList(listID: string) {
+    if (!this.REGEX.recordID.test(listID)) {
+      throw new Error('Invalid List ID');
+    }
+    const { baseID, listsTableID, accessToken } = this.settings;
+    const requestURL = `https://api.airtable.com/v0/${baseID}/${listsTableID}/${listID}`;
+    const response = await fetch(requestURL, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    const data = await response.json();
+    if (!data || !data.fields) {
+      throw new Error('List not found');
+    }
+    return new List({
+      name: data.fields.Name || 'Unnamed List',
+      id: data.id,
+      url: `https://airtable.com/${baseID}/${listsTableID}/${data.id}`,
+      links: data.fields.Links,
+    });
+  }
 
-	readCache(key: string) {
-		let record = this.cache.get(key);
-		if (record && (new Date().getTime() - record.cachedAt.getTime()) < 1000 * 60 * 3) {
-			return record;
-		}
-	}
+  readCache(key: string) {
+    const record = this.cache.get(key);
+    if (record && (new Date().getTime() - record.cachedAt.getTime()) < 1000 * 60 * 3) {
+      return record;
+    }
+  }
 
-	cache = new Map<string, CacheRecord>();
+  cache = new Map<string, CacheRecord>();
 
-	REGEX = {
-		baseID: /^app[a-zA-Z0-9]{9,}$/,
-		tableID: /^tbl[a-zA-Z0-9]{9,}$/,
-		recordID: /^rec[a-zA-Z0-9]{9,}$/,
-	};
+  REGEX = {
+    baseID: /^app[a-zA-Z0-9]{9,}$/,
+    tableID: /^tbl[a-zA-Z0-9]{9,}$/,
+    recordID: /^rec[a-zA-Z0-9]{9,}$/,
+  };
 }
 
 class SettingTab extends PluginSettingTab {
-	plugin: AirtableLinks;
+  plugin: AirtableLinks;
 
-	constructor(app: App, plugin: AirtableLinks) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: AirtableLinks) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
+    containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
-		new Setting(containerEl)
-			.setName('Airtable Personal Access Token')
-			.addText(text => text
-				.setPlaceholder('Enter your token')
-				.setValue(this.plugin.settings.accessToken)
-				.onChange(async (value) => {
-					console.log('Access Token: ' + value);
-					this.plugin.settings.accessToken = value;
-					await this.plugin.saveSettings();
-				}));
+    new Setting(containerEl)
+      .setName('Airtable Personal Access Token')
+      .addText(text => text
+        .setPlaceholder('Enter your token')
+        .setValue(this.plugin.settings.accessToken)
+        .onChange(async (value) => {
+          console.log(`Access Token: ${value}`);
+          this.plugin.settings.accessToken = value;
+          await this.plugin.saveSettings();
+        }));
 
-		new Setting(containerEl)
-			.setName('Base ID')
-			.setDesc('ID of the Airtable base')
-			.addText(text => text
-				.setPlaceholder('Enter base ID')
-				.setValue(this.plugin.settings.baseID)
-				.onChange(async (value) => {
-					value = value.split('?')[0];
-					if (!this.plugin.REGEX.baseID.test(value)) {
-						new Notice('Invalid Base ID');
-						return;
-					}
-					console.log('Base ID: ' + value);
-					this.plugin.settings.baseID = value;
-					await this.plugin.saveSettings();
-				}));
+    new Setting(containerEl)
+      .setName('Base ID')
+      .setDesc('ID of the Airtable base')
+      .addText(text => text
+        .setPlaceholder('Enter base ID')
+        .setValue(this.plugin.settings.baseID)
+        .onChange(async (value) => {
+          value = value.split('?')[0];
+          if (!this.plugin.REGEX.baseID.test(value)) {
+            new Notice('Invalid Base ID');
+            return;
+          }
+          console.log(`Base ID: ${value}`);
+          this.plugin.settings.baseID = value;
+          await this.plugin.saveSettings();
+        }));
 
-		new Setting(containerEl)
-			.setName('Lists Table ID')
-			.setDesc('ID of the Airtable table containing lists')
-			.addText(text => text
-				.setPlaceholder('Enter table ID')
-				.setValue(this.plugin.settings.listsTableID)
-				.onChange(async (value) => {
-					value = value.split('?')[0];
-					if (!this.plugin.REGEX.tableID.test(value)) {
-						new Notice('Invalid Table ID');
-						return;
-					}
-					console.log('Lists ID: ' + value);
-					this.plugin.settings.listsTableID = value;
-					await this.plugin.saveSettings();
-				}));
+    new Setting(containerEl)
+      .setName('Lists Table ID')
+      .setDesc('ID of the Airtable table containing lists')
+      .addText(text => text
+        .setPlaceholder('Enter table ID')
+        .setValue(this.plugin.settings.listsTableID)
+        .onChange(async (value) => {
+          value = value.split('?')[0];
+          if (!this.plugin.REGEX.tableID.test(value)) {
+            new Notice('Invalid Table ID');
+            return;
+          }
+          console.log(`Lists ID: ${value}`);
+          this.plugin.settings.listsTableID = value;
+          await this.plugin.saveSettings();
+        }));
 
-		new Setting(containerEl)
-			.setName('Links Table ID')
-			.setDesc('ID of the Airtable table containing links')
-			.addText(text => text
-				.setPlaceholder('Enter table ID')
-				.setValue(this.plugin.settings.linksTableID)
-				.onChange(async (value) => {
-					value = value.split('?')[0];
-					if (!this.plugin.REGEX.tableID.test(value)) {
-						new Notice('Invalid Table ID');
-						return;
-					}
-					console.log('Links ID: ' + value);
-					this.plugin.settings.linksTableID = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Links Table ID')
+      .setDesc('ID of the Airtable table containing links')
+      .addText(text => text
+        .setPlaceholder('Enter table ID')
+        .setValue(this.plugin.settings.linksTableID)
+        .onChange(async (value) => {
+          value = value.split('?')[0];
+          if (!this.plugin.REGEX.tableID.test(value)) {
+            new Notice('Invalid Table ID');
+            return;
+          }
+          console.log(`Links ID: ${value}`);
+          this.plugin.settings.linksTableID = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
